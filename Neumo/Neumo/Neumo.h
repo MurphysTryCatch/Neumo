ff394277a551fa68acd1302803a3c72288d4c374
murphystrycatch <linmurph@gmail.com>
2020-09-28 19:49:45 -0500

    add framework

---
+++ Neumo/Neumo/Neumo.h
@@ -0,0 +1,19 @@
+//
+//  Neumo.h
+//  Neumo
+//
+//  Created by Murphy, Oona on 9/28/20.
+//  Copyright © 2020 OonaMurphy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Neumo.
+FOUNDATION_EXPORT double NeumoVersionNumber;
+
+//! Project version string for Neumo.
+FOUNDATION_EXPORT const unsigned char NeumoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Neumo/PublicHeader.h>
+
+
